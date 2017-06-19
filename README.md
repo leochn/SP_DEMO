c5373d1517e3bb5747e9e082aa08350fef22ef25
leochn <appchn@163.com>
2017-06-19 14:45:08 +0800

    update md

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # SP_DEMO
+
+
+## 集成 MyBatis Generator
+
+通过 Maven 插件集成的，所以运行插件使用下面的命令：
+
+mvn mybatis-generator:generate
+Mybatis Geneator 详解:
+
+http://blog.csdn.net/isea533/article/details/42102297
--- src/main/java/com/example/controller/UserInfoController.java
+++ src/main/java/com/example/controller/UserInfoController.java
@@ -152,23 +152,4 @@ public class UserInfoController {
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
     }
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 }
