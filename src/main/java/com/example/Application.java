c2d615667bab29c8d3e528cb8f91ebcc8d3e76b5
leochn <appchn@163.com>
2017-06-19 16:26:06 +0800

    update md

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -3,11 +3,13 @@ package com.example;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
 @SpringBootApplication
 @MapperScan(basePackages = "com.example.mapper")
+//@EnableDiscoveryClient
 public class Application {
 
 	public static void main(String[] args) {
